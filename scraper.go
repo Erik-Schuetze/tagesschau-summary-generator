2c6bb37a701165657ae6dbf53b2c3b4448b9c035
Erik Schuetze <erik.schuetze99@gnail.com>
2023-12-16 19:39:09 +0100

    add more metadata

--- scraper.go
+++ scraper.go
@@ -8,15 +8,29 @@ import (
 	"github.com/gocolly/colly/v2"
 )
 
-func main() {
-	articles := getNews()
+type Article struct {
+	Headline string
+	Content  string
+	Link     string
+	Tags     []string
+}
 
-	for _, element := range articles {
-		//fmt.Println(element)
-		content := getArticleContent(element)
-		fmt.Printf("\n%s:\n%s", element, content)
+func main() {
+	articles := []Article{}
+
+	links := getNews()
+
+	for _, link := range links {
+		articles = append(articles, getArticle(link))
 	}
 
+	for _, e := range articles {
+		fmt.Println(e.Link)
+		fmt.Println(e.Headline)
+		fmt.Println(e.Tags)
+		fmt.Println(e.Content)
+		fmt.Println(" ")
+	}
 }
 
 func getNews() []string {
@@ -39,16 +53,38 @@ func getNews() []string {
 	return articles
 }
 
-func getArticleContent(link string) string {
-	articleContent := ""
+func getArticle(link string) Article {
+	article := new(Article)
+	article.Link = link
 
 	c := colly.NewCollector()
 
+	// get headline
+	c.OnHTML("div>h1>span", func(e *colly.HTMLElement) {
+		if strings.Contains(e.Attr("class"), "headline") {
+			article.Headline = e.Text
+		}
+	})
+
+	// fuse texts together
+	content := ""
 	c.OnHTML("article>p", func(e *colly.HTMLElement) {
-		articleContent = fmt.Sprintf("%s%s ", articleContent, strings.TrimSpace(e.Text))
+		if strings.Contains(e.Attr("class"), "textabsatz") {
+			content = fmt.Sprintf("%s%s ", content, strings.TrimSpace(e.Text))
+		}
+		article.Content = content
+	})
+
+	// get tags
+	tags := []string{}
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		if strings.Contains(e.Attr("class"), "tag-btn") {
+			tags = append(tags, e.Text)
+		}
+		article.Tags = tags
 	})
 
 	c.Visit(link)
 
-	return articleContent
+	return *article
 }
